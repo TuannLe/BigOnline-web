640dc285c3143c961f80d48b4795b2d0b3585feb
Tuấn Lê <tuanle37372000@gmail.com>
2023-05-06 21:27:13 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <p className="text-2xl text-red-400">Hello</p>
+    </div>
+  )
+}
+
+export default App
